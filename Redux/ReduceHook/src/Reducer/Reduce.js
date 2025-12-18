5406095e6921987f3e83d0a082df5205ccdc3878
Nilesh Yadav <ny033222@gmail.com>
2025-12-18 16:24:39 +0530

    code of the day

--- Redux/ReduceHook/src/Reducer/Reduce.js
+++ Redux/ReduceHook/src/Reducer/Reduce.js
@@ -1,1 +1,52 @@
 import { DELETE_TODO_ITEMS, ADD_TODO_ITEMS, EDITS_TODO_ITEMS} from './Action';
+import { initialValue } from './Store';
+
+export const reducer = (state = initialValue, action) => {
+    console.log('~ action:', action);
+    switch (action.type) {
+        return {
+            ...state,
+            items: [
+                ...state,
+                {
+                    id: DataTransfer.now(),
+                    Text: action.payload,
+                    isEdits: false,
+                    isComplete: false,
+                },
+            ],
+        };
+
+        case EDITS_TODO_ITEMS;
+        if (action.payload.updatedText && action.payload.id) {
+            return {
+                ...state,
+                items: state.items.map((el)=>
+                el.id === action.payload.id 
+                ?{
+                    ...el,
+                    isEdits: !el.isEdits,
+                    text: action.payload.updatedText,
+                }
+                : el
+                ),
+            };
+        } else {
+            return {
+                ...state,
+                items: state.items.map((el) =>
+                el.id === action.payload ? { ...el, isEdits: !el.isEdits } : el
+                ),
+           };
+        }
+
+        case DELETE_TODO_ITEMS:
+            return {
+                ...state,
+                items: state.items.filter((el) => el.id === action.payload),
+            };
+
+            default:
+                return state;
+    }
+};
