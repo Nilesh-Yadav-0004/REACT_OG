017c28b1f6d4eb7a1b7ec1bd197dadc0e87a474b
Nilesh Yadav <ny033222@gmail.com>
2026-01-10 17:17:56 +0530

    All Codes

---
+++ local redux/public/Auth/Action.js
@@ -0,0 +1,15 @@
+/* 
+login successfull
+login failure
+login request
+
+remove token 
+
+
+*/
+
+export const REMOVE_TOKEN = 'REMOVE_TOKEN';
+
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESSFULL = 'LOGIN_SUCCESSFULL';
