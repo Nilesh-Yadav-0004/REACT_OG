de68bacdb197bddfe2583a916ff6dbc2210e5c9a
Nilesh Yadav <ny033222@gmail.com>
2026-01-19 16:06:51 +0530

    code of the day

--- local redux/src/Reducer/AUTH/Action.js
+++ local redux/src/Reducer/AUTH/Action.js
@@ -15,3 +15,13 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 
 export const LOGIN_SUCCESSFULL = 'LOGIN_SUCCESSFULL';
+
+const getLogin = () => {
+    return { type: LOGIN_SUCCESSFULL, payload: payload };
+};
+
+const loginFailure = (payload) => {
+    return { type: LOGIN_FAILURE, payload: payload};
+};
+
+export { getLogin, LOGIN_SUCCESSFULL, loginFailure };
