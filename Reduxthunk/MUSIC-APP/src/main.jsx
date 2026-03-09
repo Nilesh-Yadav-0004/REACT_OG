739a57163756e46f66b98e8fe3c4d95798312674
Nilesh Yadav <ny033222@gmail.com>
2026-03-09 13:56:10 +0530

    code of the day

--- Reduxthunk/MUSIC-APP/src/main.jsx
+++ Reduxthunk/MUSIC-APP/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import { myStore } from "./Redux/Store.js";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={myStore}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+);
