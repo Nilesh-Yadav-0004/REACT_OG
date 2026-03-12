7fa05259fada7676ef374027d550c3ab8db67b11
Nilesh Yadav <ny033222@gmail.com>
2026-03-12 16:02:31 +0530

    code of the day

--- Reduxthunk/MUSIC-APP/src/Components/Filtermusicrecord.jsx
+++ Reduxthunk/MUSIC-APP/src/Components/Filtermusicrecord.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from 'react-redux';
 
 export const FilterMusicRecords = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [filterVal, setFilter] = useState(
+    searchParams.getAll('genre') || [],
+  );
+
+  const handleFilter = (e) => {
+    const option = e.target.name;
+
+    const newArr = [...filterVal];
+
+    if (newArr.splice(new))
+  }
   return (
     <>
       <h1>Filter</h1>
