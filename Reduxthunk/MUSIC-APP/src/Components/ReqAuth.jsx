ae6abc607d98bc587e4d959c8f455d857b4122b6
Nilesh Yadav <ny033222@gmail.com>
2026-03-19 15:37:15 +0530

    code of the day

---
+++ Reduxthunk/MUSIC-APP/src/Components/ReqAuth.jsx
@@ -0,0 +1,17 @@
+
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+
+export const ReqAuth = ({ children }) => {
+  const { isAuth } = useSelector((store) => store.auth);
+  console.log("Is Auth: ", isAuth);
+  const location = useLocation();
+
+  if (!isAuth) {
+    return (
+      <Navigate to={"/login"} state={{ from: location.pathname }} replace />
+    );
+  }
+
+  return children;
+};
