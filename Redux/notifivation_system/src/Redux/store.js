bdd8064f40fb1a5245d1647e01784c05b977bfbd
Nilesh Yadav <ny033222@gmail.com>
2026-03-25 18:28:52 +0530

    code

--- Redux/notifivation_system/src/Redux/store.js
+++ Redux/notifivation_system/src/Redux/store.js
@@ -2,3 +2,6 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from 'redux-thunk';
 import { Reducer as notificationReducer } from './reducer';
 
+const root = combineReducers({notification: notificationReducer,});
+
+export const myStore = legacy_createStore(root, )
